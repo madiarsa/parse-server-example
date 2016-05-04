b889d8541400b2d974c66c7f5874b60217dc5365
madiarsa <madiarsa@gmail.com>
2016-05-04 23:23:13 +0900

    update to parse server 2.2.7 and parse 1.8.x

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'myMasterKey',
+  serverURL: process.env.SERVER_URL,
   verifyUserEmails: true,
   emailAdapter: {
     module: 'parse-server-simple-mailgun-adapter',
