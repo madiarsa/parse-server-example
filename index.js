2f5f968c7564395615d4190fbdb1f4387da73bf3
madiarsa <Mac@MacBook.local>
2016-05-04 15:44:24 +0900

    add sendgrid adapter

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-
+var SimpleSendGridAdapter = require('parse-server-sendgrid-adapter');
+ 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI
 
 if (!databaseUri) {
@@ -14,7 +15,11 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myMasterKey'
+  masterKey: process.env.MASTER_KEY || 'myMasterKey',
+  emailAdapter: SimpleSendGridAdapter({
+    apiKey: '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',
+    fromAddress: ‘madiarsa@gmail.com’
+  })
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
